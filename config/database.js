6896c0aafdf4b0615f4f497a24989c15818131ae
Klyamanin Maxim <maxim@sendev.gmbh>
2026-03-23 15:17:20 +0300

    chore: switch local backend to postgresql by default

--- config/database.js
+++ config/database.js
@@ -1,30 +1,18 @@
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  if (client === "postgres") {
-    return {
-      connection: {
-        client,
-        connection: {
-          host: env("DATABASE_HOST", "127.0.0.1"),
-          port: Number(env("DATABASE_PORT", "5432")),
-          database: env("DATABASE_NAME", "shop_kardi"),
-          user: env("DATABASE_USERNAME", "shop_kardi"),
-          password: env("DATABASE_PASSWORD", "shop_kardi"),
-          ssl: env("DATABASE_SSL", "false") === "true"
-        }
-      }
-    };
-  }
+  const client = env("DATABASE_CLIENT", "postgres");
 
   return {
     connection: {
-      client: "sqlite",
+      client,
       connection: {
-        filename: env("DATABASE_FILENAME", ".tmp/data.db")
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: Number(env("DATABASE_PORT", "5432")),
+        database: env("DATABASE_NAME", "shop_kardi"),
+        user: env("DATABASE_USERNAME", "shop_kardi"),
+        password: env("DATABASE_PASSWORD", "shop_kardi"),
+        ssl: env("DATABASE_SSL", "false") === "true"
       },
-      useNullAsDefault: true
+      debug: env("DATABASE_DEBUG", "false") === "true"
     }
   };
 };
-
