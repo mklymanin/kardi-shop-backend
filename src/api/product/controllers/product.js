8c9a8ce8ab691f89e0bb7ba5bb5b307e063b4351
cptblackmore <redshatterr@gmail.com>
2026-04-06 14:40:41 +0300

    feat: add store filter to product query for published items

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -2,4 +2,14 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::product.product");
+function ensureStoreFilters(query) {
+  query.filters ??= {};
+  query.filters.isPublishedToStore = { $eq: true };
+}
+
+module.exports = createCoreController("api::product.product", () => ({
+  async find(ctx) {
+    ensureStoreFilters(ctx.query);
+    return await super.find(ctx);
+  },
+}));
